c4f7e1a9316684bf2c4fdd9b9ff1741e03f4add5
lovelytong <tongyufujing@qq.com>
2018-07-16 18:28:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="header">
+      <el-row class="header-container">
+        <el-col class="header-title" :span="4">
+          <div>PROJECT<span style="color: cornflowerblue">BOX</span></div>
+        </el-col>
+        <el-col :span="12" class="header-title">
+          <div></div>
+        </el-col>
+        <el-col class="header-icon" :span="3">
+          <div><i class="el-icon-tickets"></i></div>
+          <div><i class="el-icon-message"></i></div>
+          <div><i class="el-icon-bell"></i></div>
+        </el-col>
+        <el-col class="header-admin" :span="3">
+          <div><i class="el-icon-tickets"></i></div>
+          <div>Project Admin</div>
+          <div><i class="el-icon-arrow-down"></i></div>
+        </el-col>
+      </el-row>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +31,47 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .header {
+    background-color: white;
+  }
+
+  .header-container {
+    margin: 30px 50px 0 50px;
+  }
+
+  .header-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60px;
+    border-right: 2px solid #eeeeee;
+  }
+
+  .header-icon {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 60px;
+    border-right: 2px solid #eeeeee;
+    font-size: 30px;
+  }
+
+  .header-admin {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 60px;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <el-row >
+    <el-col :span="5">
+      <div class="menu-background">
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#658bb1"
+          text-color="#fff"
+          active-text-color="#47c5f0">
+          <el-menu-item>
+            <i class="el-icon-menu"></i>
+            <span slot="title">Dashboard</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-document"></i>
+            <span slot="title">Calendar</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-setting"></i>
+            <span slot="title">Tasks</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-menu"></i>
+            <span slot="title">File Manager</span>
+          </el-menu-item>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Team</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">Manage</el-menu-item>
+              <el-menu-item index="1-2">Groups</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-menu-item>
+            <i class="el-icon-menu"></i>
+            <span slot="title">Time Logger</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-document"></i>
+            <span slot="title">Discuss</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-setting"></i>
+            <span slot="title">Tickets</span>
+          </el-menu-item>
+          <el-menu-item>
+            <i class="el-icon-setting"></i>
+            <span slot="title">Accounting</span>
+          </el-menu-item>
+        </el-menu>
+      </div>
+    </el-col>
+    <el-col :span="19" class="main">
+      <div class="white-background margin-50 border-radius-5" style="height: 30px;">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="white-background margin-50 border-radius-5">
+        <el-table
+          :data="tableData"
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)"
+                type="warning"
+              >Edit
+              </el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">Romove
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -88,7 +109,31 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
@@ -96,18 +141,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .main {
+    background-color: #eeeeee;
+    height: 850px;
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+
+  .white-background {
+    background-color: white;
+  }
+
+  .margin-50 {
+    margin-top: 50px;
+  }
+
+  .border-radius-5 {
+    border-radius: 5px;
+  }
+
+  .menu-background {
+    background-color: white;
+    padding-left: 50px;
+  }
+  .el-menu-vertical-demo {
+    height: 850px;
+  }
+
 </style>
